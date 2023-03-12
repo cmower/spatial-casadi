cfaf61f759f7194c5dc21efe3acc74bf766dfef5
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-03-12 23:09:36 +0000

    Add long description and update license and description

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="spatial-casadi",
-    description="",
+    description="Spatial transformation library for CasADi Python.",
     version="1.0.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setup(
     },
     author="Christopher E. Mower",
     author_email="christopher.mower@kcl.ac.uk",
-    license="Apache License, Version 2.0",
+    license="LGPL-3.0 license",
     packages=["spatial_casadi"],
     install_requires=[
         "numpy",
