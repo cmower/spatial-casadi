7f593df003626c24dd1c00ae0ef220033aeb266c
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2024-10-24 13:22:15 +0800

    Add new angles.py script

---
+++ spatial_casadi/angles.py
@@ -0,0 +1,31 @@
+import casadi as cs
+
+# The number pi (i.e. 3.141..)
+pi = cs.np.pi
+
+
+def deg2rad(x):
+    """Convert degrees to radians.
+
+    Args
+      x (array-like) An array containing angles in degrees.
+
+    Returns
+      casadi-array
+        An array containing angles in radians.
+
+    """
+    return (pi / 180.0) * cs.horzcat(x)
+
+
+def rad2deg(x):
+    """Convert radians to degrees.
+
+    Args
+      x (array-like) An array containing angles in radians.
+
+    Returns
+      casadi-array
+        An array containing angles in degrees.
+    """
+    return (180.0 / pi) * cs.horzcat(x)
